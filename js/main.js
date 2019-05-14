a66f0450118291a9a17df0ba0a46a53cbc186a97
SimoTr84 <simone.trucco@gmail.com>
2019-05-14 18:23:32 +1000

    finalised hero and about - started with projects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+//Hamburger Animation
 $(document).ready(function(){
     $(".hamburger").click(function(){
         $(this).parent().toggleClass('open');
     });
 });
+
+// Parallax.js
+$(document).ready(function(){
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+});
+
+
