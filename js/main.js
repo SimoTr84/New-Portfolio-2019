c5daa8f2ed28b2e52f1084bfb258391e2ed62298
SimoTr84 <simone.trucco@gmail.com>
2019-05-19 18:45:43 +1000

    finalised GA Projects section - Finalised footer section - added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -11,4 +11,9 @@ $(document).ready(function(){
 //     var parallaxInstance = new Parallax(scene);
 // });
 
-
+// Scroll certain amounts from current position
+window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+});
